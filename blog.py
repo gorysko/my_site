08fca34291597733a0417861e6d62c1c05a051d8
gorysko <ihor.lushchyk@sethq.com>
2014-03-28 16:10:38 +0200

    pep8

--- blog.py
+++ blog.py
@@ -5,10 +5,10 @@ from flask.ext.flatpages import FlatPages
 from flask.ext.flatpages import pygments_style_defs
 from flask_frozen import Freezer
 from argh import *
-import time
+
 
 # Configuration
-BASE_URL = "http://evenchick.com"
+BASE_URL = "http://wwww.gorysko.com"
 DEBUG = True                # enable debugging mode
 FLATPAGES_AUTO_RELOAD = DEBUG   # prevent caching of pages
 FLATPAGES_EXTENSION = ".md"
@@ -18,50 +18,58 @@ FREEZER_IGNORE_MIMETYPE_WARNINGS = True
 FREEZER_DEFAULT_MIMETYPE = "text/html"
 
 # Set up Flask
-app = Flask(__name__)
-app.config.from_object(__name__)
+APP = Flask(__name__)
+APP.config.from_object(__name__)
 
 # Set up Flask extensions
-assets = Environment(app)
-markdown = Markdown(app)
-pages = FlatPages(app)
-freezer = Freezer(app)
+ASSETS = Environment(APP)
+MARKDOWN = Markdown(APP)
+PAGES = FlatPages(APP)
+FREEZER = Freezer(APP)
 
 # Helper functions
 def get_pages_by_type(page_type):
-    page_list = list(pages)
-    matches = [p for p in page_list if p.meta.get('type') == page_type]
+    """Gets page path."""
+    page_list = list(PAGES)
+    matches = [source for source in page_list \
+                if source.meta.get('type') == page_type]
     return matches
 
 # Flask Routing
-@app.route("/")
+@APP.route("/")
 def home():
+    """Renders home page."""
     post_list = get_pages_by_type('post')
     latest = sorted(post_list, reverse=True, key=lambda p: p.meta['date'])
     return render_template("index.html", posts=latest[:5])
 
-@app.route("/<path:path>/")
+@APP.route("/<path:path>/")
 def page(path):
-    page = pages.get_or_404(path)
-    return render_template("page.html", page=page)
+    """Renders page."""
+    source = PAGES.get_or_404(path)
+    return render_template("page.html", page=source)
 
-@app.route("/portfolio/")
+@APP.route("/portfolio/")
 def portfolio():
+    """Renders portfolio page"""
     portfolio_pages = get_pages_by_type('portfolio')
-    date_sorted = sorted(portfolio_pages, reverse=True, key=lambda p: p.meta['date'])
+    date_sorted = sorted(portfolio_pages, reverse=True,
+                         key=lambda p: p.meta['date'])
     return render_template("portfolio.html", items=date_sorted)
 
-@app.route('/pygments.css')
+@APP.route('/pygments.css')
 def pygments_css():
+    """Renders css."""
     return pygments_style_defs('tango'), 200, {'Content-Type': 'text/css'}
 
 
 # URL Generators
 
-@freezer.register_generator
+@FREEZER.register_generator
 def page_generator():
-    for page in list(pages):
-        yield "/" + str(page.path)
+    """Generaes page."""
+    for source in list(PAGES):
+        yield ("/%s" % str(source.path))
 
 # CLI Commands
 @command
@@ -69,15 +77,15 @@ def serve(server="127.0.0.1", port=8080, debug=DEBUG):
     """ Start a server to run the site
         default: 127.0.0.1:8080
     """
-    app.run(host=server, port=port, debug=debug)
+    APP.run(host=server, port=port, debug=debug)
 
 
 @command
 def build():
     """ Generate a static version of the site
     """
-    app.debug = False
-    freezer.freeze()
+    APP.debug = False
+    FREEZER.freeze()
 
 if __name__ == "__main__":
     parser = ArghParser()
